96f26993041b89462d4061dbb4467f782ad67aa5
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 12:58:14 -0300

    Create FatorialRecursivo.java

---
+++ estruturas-e-algoritmos/recursividade/FatorialRecursivo.java
@@ -0,0 +1,12 @@
+public class FatorialRecursivo {
+    public static void main(String[] args) {
+        System.out.println(fatorial(5));
+    }
+
+    static int fatorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * fatorial(n - 1);
+    }
+}
