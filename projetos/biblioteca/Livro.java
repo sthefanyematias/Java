223fe7e6315214f8b971acf40a200e6d58342bc8
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 13:03:12 -0300

    Create Livro.java

---
+++ projetos/biblioteca/Livro.java
@@ -0,0 +1,11 @@
+public class Livro {
+    String titulo;
+    String autor;
+    boolean alugado;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.alugado = false;
+    }
+}
