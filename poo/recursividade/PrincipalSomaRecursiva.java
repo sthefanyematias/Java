0bfa639cc714e3524d9a03af62edbac5e3da14e1
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 11:42:52 -0300

    Create PrincipalSomaRecursiva.java

---
+++ poo/recursividade/PrincipalSomaRecursiva.java
@@ -0,0 +1,8 @@
+public class PrincipalSomaRecursiva {
+    public static void main(String[] args) {
+        int[] v = {1, 2, 3, 4};
+
+        SomaRecursiva s = new SomaRecursiva();
+        System.out.println(s.somar(v, 0));
+    }
+}
