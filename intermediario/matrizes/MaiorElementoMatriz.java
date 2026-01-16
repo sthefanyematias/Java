570c1705f3a8f6b328dc6f46140a827ce14ffdfe
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 09:54:07 -0300

    Create MaiorElementoMatriz.java

---
+++ intermediario/matrizes/MaiorElementoMatriz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MaiorElementoMatriz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        int maior = matriz[0][0];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] > maior) {
+                    maior = matriz[i][j];
+                }
+            }
+        }
+
+        System.out.println(maior);
+        scanner.close();
+    }
+}
