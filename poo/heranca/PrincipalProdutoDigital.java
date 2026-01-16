3e71effa445d045168a064ca308c110026265d1e
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 11:22:44 -0300

    Create PrincipalProdutoDigital.java

---
+++ poo/heranca/PrincipalProdutoDigital.java
@@ -0,0 +1,8 @@
+public class PrincipalProdutoDigital {
+    public static void main(String[] args) {
+        ProdutoDigital p = new ProdutoDigital("Curso Java", 99.9, "PDF");
+
+        p.mostrarProduto();
+        p.mostrarFormato();
+    }
+}
