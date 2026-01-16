2a819af353f618dfb321d28e988e1ce42c4b7005
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 11:01:43 -0300

    Create PrincipalPessoa.java

---
+++ poo/fundamentos/PrincipalPessoa.java
@@ -0,0 +1,9 @@
+public class PrincipalPessoa {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        p.nome = "Sthefany";
+        p.idade = 20;
+
+        p.mostrarDados();
+    }
+}
