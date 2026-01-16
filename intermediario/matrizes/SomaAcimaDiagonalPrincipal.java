6ca191a8a5d75d63cc83b73ae98e0b80dc56635e
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 10:02:27 -0300

    Create SomaAcimaDiagonalPrincipal.java

---
+++ intermediario/matrizes/SomaAcimaDiagonalPrincipal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SomaAcimaDiagonalPrincipal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = scanner.nextInt();
+                if (j > i) {
+                    soma += matriz[i][j];
+                }
+            }
+        }
+
+        System.out.println(soma);
+        scanner.close();
+    }
+}
