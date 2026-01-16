b63880a067d52b9b44f942506c0870faa0b8a962
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-16 09:23:53 -0300

    Add SelecionaDiaSemana to select week days

---
+++ basico/condicionais/SelecionaDiaSemana.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SelecionaDiaSemana {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dia = scanner.nextInt();
+
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terca");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Dia invalido");
+        }
+
+        scanner.close();
+    }
+}
